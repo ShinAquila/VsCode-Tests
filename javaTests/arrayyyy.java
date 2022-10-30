903e2c861fd8301db699d009cb8f5b97c895efbf
ShinAquila <augusto08rivera12@gmail.com>
2022-10-30 19:11:55 +0800

    git commit

--- javaTests/arrayyyy.java
+++ javaTests/arrayyyy.java
@@ -6,18 +6,15 @@ public class arrayyyy {
     {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter num");
-        int num = scan.nextInt();
-        num = num+1;
+        int num = scan.nextInt()+1;
 
         System.out.println("");
+        scan.close();
 
         int[] a = new int[num];
 
-        for(int i=1; i<num; ++i){
+        for(int i=1; i<num; i++){
             a[i] = i;
-        }
-
-        for(int i=1; i<num; ++i){
             System.out.println(a[i]);
         }
 
@@ -27,6 +24,6 @@ public class arrayyyy {
             
         }
         System.out.println("Total: "+sum);
-        
+        System.exit(0);
     }
 }
--- javaTests/calcu.java
+++ javaTests/calcu.java
@@ -5,51 +5,46 @@ public class calcu {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Scanner operator = new Scanner(System.in);
-        float num1, num2;
+        int num1, num2;
         
-        while(true){
-            String nomore = scan.nextLine();
-            if(nomore.equals("C")){
-                System.out.println("Input First Number:");
-                num1 = scan.nextFloat();
-    
-                System.out.println("Input Second Number:");
-                num2 = scan.nextFloat();
-    
-                System.out.println("Input Operator:");
-                String ope = operator.nextLine();
-    
-    
-                    if(ope.equals("+")){
-                        System.out.println("Result is "+(num1 + num2));
-                    }
-    
-                    else if(ope.equals("-")){
-                        System.out.println("Result is "+(num1 - num2));
-                    }
-    
-                    else if(ope.equals("*")){
-                        System.out.println("Result is "+(num1 * num2));
-                    }
-    
-                    else if(ope.equals("/")){
-                        System.out.println("Result is "+(num1 / num2));
-                    }
-    
-                    else{
-                        System.out.println("Error");
-                    }
-    
-                    System.out.println("---------------------------");
+            System.out.println("Input First Number:");
+            num1 = scan.nextInt();
 
-            } 
-            else if(nomore.equals("STOP")){
-                break;
-            }
+            System.out.println("Input Second Number:");
+            num2 = scan.nextInt();
+
+            System.out.println("Input Operator:");
+            String ope = operator.nextLine();
+
+
+                if(ope.equals("+")){
+                    System.out.println("Result is "+(num1 + num2));
+                }
+
+                else if(ope.equals("-")){
+                    System.out.println("Result is "+(num1 - num2));
+                }
+
+                else if(ope.equals("*")){
+                    System.out.println("Result is "+(num1 * num2));
+                }
+
+                else if(ope.equals("/")){
+                    System.out.println("Result is "+(num1 / num2));
+                }
+
+                else{
+                    System.out.println("Error");
+                }
+
+                System.out.println("---------------------------");
+                scan.close();
+                operator.close();
+
             
         }
         
 
     }
-}
+
 
