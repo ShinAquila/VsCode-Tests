ff5de3814dc7c06915135a19515f3730cf2d6a77
ShinAquila <augusto08rivera12@gmail.com>
2022-11-15 15:45:51 +0800

    init

---
+++ CCE/arrayJo.java
@@ -0,0 +1,26 @@
+package CCE;
+import java.util.Scanner;
+
+public class arrayJo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String names [] = {"jo","an","de","sa"};
+        Boolean detect = false;
+
+        System.out.println("Enter Name ");
+        String nameInput = scan.nextLine();
+
+        for(int i=0; i<names.length; i++){
+            if(nameInput.equals(names[i])){
+                detect = true;
+            }
+        }
+
+        if (detect != true) {
+            System.out.println("User Not Found");
+        } else {
+            System.out.println("User Found");
+        }
+        scan.close();
+    }
+}
