7a2d592fe62776060fad4f4f714671f6e6788dcd
ShinAquila <augusto08rivera12@gmail.com>
2022-11-20 13:15:15 +0800

    init

--- CCE/arrayExercises/advExercise02.java
+++ CCE/arrayExercises/advExercise02.java
@@ -9,13 +9,13 @@ public class advExercise02 {
         String names [] = {"Clariedhel","Jayvive","Weelyn","Lorre","Mae Ann","Nelson","Mervin","Jay","Ryan","Eric","Michael","Joy","Angelo","Marisol","Kent","Vincent"};
 
         String cont = "";
+        boolean detect = false;
 
         System.out.println("SEARCH ARRAY CODE");
         System.out.println("This is a system that can search the name of the student of ICT-TVL-B9");
         System.out.println("**************************************************************************");
 
         do {
-            boolean detect = false;
             System.out.print("Please enter the Student Name:      ");
             String nameInput = scan.nextLine();
             System.out.println("--------------------------------------------------------------------------");
