30d7efacc806663dbbc454df994fb7f5d6f02e0b
ShinAquila <augusto08rivera12@gmail.com>
2022-11-20 14:40:01 +0800

    init

--- ProjectTest/msgD.java
+++ ProjectTest/msgD.java
@@ -10,6 +10,13 @@ public class msgD {
         String[] options = {"I adore turtles", "Yes", "Maybe", "Urm...", "No", "Hate them"};
         ImageIcon icon = new ImageIcon("src/images/turtle32.png");
         String n = (String)JOptionPane.showInputDialog(null, "Do you like turtles??","I like turtles", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
-        System.out.println(n);
+
+        if (n == options[5]) {
+            JOptionPane.showMessageDialog(null, "Well so be it then....");
+        } else if (n == options[0]) {
+            JOptionPane.showMessageDialog(null, "Finally a man of culture");
+        } else{
+            JOptionPane.showMessageDialog(null, "Ok then");
+        }
     }
 }
