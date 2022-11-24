26034d0a5c4b39301b90f7d7de3e7b39b32ca236
ShinAquila <augusto08rivera12@gmail.com>
2022-11-23 19:24:51 -0800

    init

--- ArrayPractice/PractFourPart4.java
+++ ArrayPractice/PractFourPart4.java
@@ -4,7 +4,6 @@ public class PractFourPart4 {
     public static void main(String[] args) {
 
         String star = "*";
-        System.out.print(star);
         
         for (int i = 0; i < 5; i++) {
             int j = 0;
@@ -13,16 +12,13 @@ public class PractFourPart4 {
                 System.out.print(" ");
             }
             for (j = 0; j <= i; j++) {
-                if (j==1) {
-                    System.out.print(star.replace(star, ""));
-                }
                 if (j != 0) {
                     System.out.print(star);
                 }
             }
-            
-            
 
+            
+            
             System.out.println();
         }
     }
