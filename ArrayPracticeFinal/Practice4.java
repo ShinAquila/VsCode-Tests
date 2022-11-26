797b6b2f981105b2cfb0628a28639a951c3f3c6b
ShinAquila <augusto08rivera12@gmail.com>
2022-11-26 09:42:21 +0800

    init

--- ArrayPracticeFinal/Practice4.java
+++ ArrayPracticeFinal/Practice4.java
@@ -1,4 +1,4 @@
-package ArrayPract;
+package ArrayPracticeFinal;
 
 public class Practice4 {
     public static void main(String[] args) {
@@ -11,20 +11,22 @@ public class Practice4 {
             }
             System.out.println();
         }
-
+        System.out.println();
+        
         //Left Triangle Star Pattern
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(star);
             }
-            System.out.println();
+            if (i != 0) {
+                System.out.println();
+            }
         }
+        System.out.println();
 
         //Right Triangle Star Pattern
+        System.out.print(star);
         for (int i = 0; i <= 5; i++) {
-            if (i == 1) {
-                System.out.print(star.replace(star, "*"));
-            }
 
             for (int j = 0; j < 5-i; j++) {
                 System.out.print(" ");
@@ -35,7 +37,10 @@ public class Practice4 {
                     System.out.print(star);
                 }
             }
-            System.out.println();
+
+            if (i != 0) {
+                System.out.println();
+            }
         }
     }
 }
