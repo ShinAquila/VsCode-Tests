1a0784660a758cf4272eb53dd38e8022e992fc56
ShinAquila <augusto08rivera12@gmail.com>
2022-12-02 20:01:35 +0800

    init

---
+++ CCEPrac/receiptTestt.java
@@ -0,0 +1,12 @@
+package CCEPrac;
+import java.util.Scanner;
+
+public class receiptTestt {
+    public static void main(String[] args) {
+        System.out.println("******************************");
+        System.out.printf("%5s %n","The IDFK Store");
+        System.out.println("******************************");
+
+
+    }
+}
