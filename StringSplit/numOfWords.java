7156d6607539e1621c4fa4b3bfee979a74e229ca
ShinAquila <augusto08rivera12@gmail.com>
2022-12-14 22:05:25 +0800

    init

--- StringSplit/numOfWords.java
+++ StringSplit/numOfWords.java
@@ -1,5 +1,19 @@
 package StringSplit;
+import java.util.StringTokenizer;
 
 public class numOfWords {
-    
+    public static void main(String[] args) {
+        String words = "You@are@here@in@this@place";
+        String wordArr[] = words.split("@");
+
+        StringTokenizer wrd = new StringTokenizer(words);
+        System.out.println("There are "+wrd.countTokens()+" words");
+
+        System.out.println();
+        // System.out.println("There are "+wordArr.length+" words");
+
+        for (int i = 0; i < wordArr.length; i++) {
+            System.out.println(wordArr[i]);
+        }
+    }
 }
